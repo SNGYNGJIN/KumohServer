7a40764602052253a6837c2a418e2dcbe0e37aed
DongInSong <songdongin97@gmail.com>
2022-12-06 18:52:56 +0900

    1206

--- src/api/busLocationApi.js
+++ src/api/busLocationApi.js
@@ -11,27 +11,67 @@ const option = '&pageNo=1&_type=json&numOfRows=10&_type=xml&cityCode=37050&route
 
 const httpsAgent = new https.Agent({
 	rejectUnauthorized: false,
-});
+});	
 
 var loadedData = {};
 getTargetCount(route);
 
 // 30초마다 불러오기
-const job = schedule.scheduleJob('*/2 * * * *', async function () {
+// const job = schedule.scheduleJob('*/2 * * * *', async function () {
+const job = schedule.scheduleJob('*/30 * * * * *', async function () {
+// console.log = (() => {
+// 	var console_log = console.log;
+// 	var timeStart = new Date().getTime();
+	
+// 	return function() {
+// 	  var delta = new Date().getTime() - timeStart;
+// 	  var args = [];
+// 	  args.push((delta / 1000).toFixed(2) + ':');
+// 	  for(var i = 0; i < arguments.length; i++) {
+// 		args.push(arguments[i]);
+// 	  }
+// 	  console_log.apply(console, args);
+// 	};
+//   })();
+  
+//   axios.interceptors.request.use( req => {
+// 	req.meta = req.meta || {}
+// 	req.meta.requestStartedAt = new Date().getTime();
+// 	return req;
+//   });
+  
+//   axios.interceptors.response.use(res => {
+// 	console.log(`Execution time for: ${res.config.url}\n - ${ new Date().getTime() - res.config.meta.requestStartedAt} ms`)
+// 	return res;
+//   },
+//   res => {
+// 	console.error(`Execution time for: ${res.config.url}\n - ${new Date().getTime() - res.config.meta.requestStartedAt} ms`)
+// 	throw res;
+//   });
 	getTargetCount(route);
 });
+// async function getTargetCount(route) {
+// 	const total = url + key + option;
+// 	// Object.keys(route).forEach(async (i) => {
+// 		for(let i = 0; i < Object.keys(route).length; i++){
+// 		const res = axios.get(total + route, { httpsAgent });
+// 		loadedData[route[i]['routeid']] = res.data;
+// 		}
+// 		// console.log(loadedData[route[i]['routeid']]);
+// 	// });
+// }
+
 
 async function getTargetCount(route) {
 	Object.keys(route).forEach(async (i) => {
 		const res = await APICall(route[i]['routeid']);
 		loadedData[route[i]['routeid']] = res.data;
-		// console.log(loadedData[route[i]['routeid']]);
 	});
 	console.log('Bus Location Information Loaded');
 }
+
 function APICall(param) {
 	const total = url + key + option;
-
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			resolve(axios.get(total + param, { httpsAgent }))
